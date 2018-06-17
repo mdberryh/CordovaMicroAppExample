b16ef17ed2e308224dd2dc23fc944baab5e6b6a4
CyberSec <mdberryh@mtu.edu>
2018-06-17 16:21:27 -0400

    Web Browser text file write and reading now working

--- Cordova/MicroAppExample/www/js/index.js
+++ Cordova/MicroAppExample/www/js/index.js
@@ -30,6 +30,8 @@ var app = {
         this.receivedEvent('deviceready');
 
         this.customGetFileExample();
+
+        this.writeToFile('example.json', { foo: 'bar' });
     },
 
     // Update DOM on a Received Event
@@ -98,7 +100,116 @@ var app = {
             }, function (err) { console.error('error getting file! ' + err); });
         }, function (err) { console.error('error getting persistent fs! ' + err); });
 
+    },
+    writeToFile: function (fileName, data) {
+
+        window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+
+        var errorHandler = function (fileName, e) {
+            var msg = '';
+
+            switch (e.code) {
+                case FileError.QUOTA_EXCEEDED_ERR:
+                    msg = 'Storage quota exceeded';
+                    break;
+                case FileError.NOT_FOUND_ERR:
+                    msg = 'File not found';
+                    break;
+                case FileError.SECURITY_ERR:
+                    msg = 'Security error';
+                    break;
+                case FileError.INVALID_MODIFICATION_ERR:
+                    msg = 'Invalid modification';
+                    break;
+                case FileError.INVALID_STATE_ERR:
+                    msg = 'Invalid state';
+                    break;
+                default:
+                    msg = 'Unknown error';
+                    break;
+            };
+
+            console.log('Error (' + fileName + '): ' + msg);
+        }
+
+        data = JSON.stringify(data, null, '\t');
+
+        function writeFile(fs) {
+
+            fs.root.getFile('log.txt', {create: true}, function(fileEntry) {
+          
+              // Create a FileWriter object for our FileEntry (log.txt).
+              fileEntry.createWriter(function(fileWriter) {
+          
+                fileWriter.onwriteend = function(e) {
+                  console.log('Write completed.');
+                };
+          
+                fileWriter.onerror = function(e) {
+                  console.log('Write failed: ' + e.toString());
+                };
+          
+                // Create a new Blob and write it to log.txt.
+                var blob = new Blob(['Lorem Ipsum'], {type: 'text/plain'});
+          
+                fileWriter.write(blob);
+          
+              }, errorHandler);
+          
+            }, errorHandler);
+          
+          }
+          function onInitFs(fs) {
+
+            fs.root.getFile('log.txt', {}, function(fileEntry) {
+          
+              // Get a File object representing the file,
+              // then use FileReader to read its contents.
+              fileEntry.file(function(file) {
+                 var reader = new FileReader();
+          
+                 reader.onloadend = function(e) {
+                   var txtArea = document.createElement('textarea');
+                   txtArea.value = this.result;
+                   console.log(this.result);
+                   
+                   document.body.appendChild(txtArea);
+                 };
+          
+                 reader.readAsText(file);
+              }, errorHandler);
+          
+            }, errorHandler);
+          
+          }
+          
+          
+          
+          window.requestFileSystem(window.PERSISTENT, 1024*1024, writeFile, errorHandler);
+          window.requestFileSystem(window.PERSISTENT, 1024*1024, onInitFs, errorHandler);
+
+        // window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (directoryEntry) {
+        //     directoryEntry.getFile(fileName, { create: true }, function (fileEntry) {
+        //         fileEntry.createWriter(function (fileWriter) {
+        //             fileWriter.onwriteend = function (e) {
+        //                 // for real-world usage, you might consider passing a success callback
+        //                 console.log('Write of file "' + fileName + '"" completed.');
+        //             };
+
+        //             fileWriter.onerror = function (e) {
+        //                 // you could hook this up with our global error handler, or pass in an error callback
+        //                 console.log('Write failed: ' + e.toString());
+        //             };
+
+        //             var blob = new Blob([data], { type: 'text/plain' });
+        //             fileWriter.write(blob);
+        //         }, errorHandler.bind(null, fileName));
+        //     }, errorHandler.bind(null, fileName));
+        // }, errorHandler.bind(null, fileName));
     }
+
+
+
 };
 
 app.initialize();
